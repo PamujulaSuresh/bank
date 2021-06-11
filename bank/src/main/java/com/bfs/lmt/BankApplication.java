c73874976af78f5ca5de7e51226e642e3716df26
SureshPamujula <pamujula.suresh@gmail.com>
2021-06-12 02:20:49 +0530

    bank registration poc code

---
+++ bank/src/main/java/com/bfs/lmt/BankApplication.java
@@ -0,0 +1,19 @@
+package com.bfs.lmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@RestController
+@SpringBootApplication
+public class BankApplication {
+	/*
+	@RequestMapping(value = "/{userName}")
+	public String readingList(){
+	  return "Spring in Action (Manning), Cloud Native Java (O'Reilly), Learning Spring Boot (Packt)";
+	}
+	*/
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+
+}
